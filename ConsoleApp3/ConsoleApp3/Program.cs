587ca6963e05c52c8f5dab2f57d340b0eaa8c38e
kiryusha42 <kirillstefashin17@gmail.com>
2026-03-05 15:28:16 +0700

    Add files via upload

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,107 +1,75 @@
-﻿using System;
+﻿using System.Reflection.Metadata.Ecma335;
 
-namespace GuessNumber
+namespace TwoPointers_SlidingWindows
 {
     internal class Program
     {
-        int min = 0;
-        int max = 0;
-        int count = 0;
-        int countGame = 0;
-        int left = 0;
-        int right = 100;
-        int counter = 0;
-
-        public int? GetNumber()
+        static void Main(string[] args)
         {
-            int attempt = 0;
-            for (int i = 0; i < 3; i++)
+            string str1 = "asd dsa";
+            string str2 = "asdsa";
+            string str3 = "asd";
+
+            Console.WriteLine(BadSolution1(str1));
+            Console.WriteLine(BadSolution1(str2));
+            Console.WriteLine(BadSolution1(str3));
+            Console.WriteLine();
+            Console.WriteLine(GoodSolution1(str1));
+            Console.WriteLine(GoodSolution1(str2));
+            Console.WriteLine(GoodSolution1(str3));
+            int[] arr = { 1, 2, 1, 4, 5, 2 };
+            Console.WriteLine(GoodSolution2(arr, 10));
+        }
+
+        public static bool BadSolution1(string str) // 0(n) - линейная сложность
+        {
+            if (string.IsNullOrWhiteSpace(str)) return false;
+
+            string rev = "";
+
+            foreach (var ch in str)
             {
-                if (int.TryParse(Console.ReadLine(), out attempt))
+                rev = ch + rev;
+            }
+            for (int i = 0; i < str.Length; i++) // 0(n)
+            {
+                if (rev[i] != str[i])
                 {
-                    if (attempt >= left && attempt <= right)
-                        return attempt;
-                    else
-                        Console.WriteLine($"Input number from [{left}];{right}]");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool GoodSolution1(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) return false;
+
+            for (int left = 0, right = str.Length - 1; left < right; left++, right--)
+            {
+                if (str[left] != str[right]) return false;
+            }
+            return true;
+        }
+
+        public static int GoodSolution2(int[] arr, int k)
+        { // 1 2  10 k=100
+            int top = 0, bottom = 0;
+            int sum = 0, max = 0;
+            while (top < arr.Length - 1)
+            {
+                if (sum + arr[top]<=k)
+                {
+                    sum += arr[top];
+                    top++;
                 }
                 else
                 {
-                    Console.WriteLine($"Input number from [{left}];{right}]");
+                    max = max > top - bottom ? max : top - bottom;
+                    sum -= arr[bottom];
+                    bottom++;
                 }
-
-                if (i == 2)
-                {
-                    Console.WriteLine("You are stupid");
-                    return null;
-                }
-            }
-            return null;
-        }
-
-        public bool CompareNumber(int attempt, int number)
-        {
-            if (number < attempt)
-            {
-                Console.WriteLine("Your number is greater");
-                return false;
-            }
-            else if (number > attempt)
-            {
-                Console.WriteLine("Your number is less");
-                return false;
-            }
-            else
-            {
-                Console.WriteLine("You are win!!!");
-                return true;
-            }
-        }
-
-        public void PlayGame(int number)
-        {
-            while (true)
-            {
-                Console.WriteLine($"Input number from [{left}];{right}]");
-                int? result = GetNumber();
-                if (result == null)
-                    return;
-
-                int attempt = result.Value;
-                counter++;
-                bool flag = CompareNumber(attempt, number);
-                if (flag == true)
-                {
-                    // Статистика игры
-                    if (min == 0 || min > counter) min = counter;
-                    max = max < counter ? counter : max;
-                    count += counter;
-                    countGame++;
-                    break;
-                }
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            Random rnd = new Random();
-            Program program = new Program();
-
-            string name = "new Name";
-            Console.WriteLine(name);
-
-            char answer = 'Y';
-            do
-            {
-                int number = rnd.Next(program.left, program.right + 1);
-                program.counter = 0; // Сброс счетчика для новой игры
-                program.PlayGame(number);
-
-                Console.WriteLine("Do you want play again?");
-                answer = Convert.ToChar(Console.ReadLine());
-
-            } while (answer == 'Y' || answer == 'y');
-
-            Console.WriteLine($"min = {program.min} max = {program.max} avg = {(double)program.count / program.countGame}");
-        }
+            } return max;
+        }   
     }
 }
